f862bb2a352fb27bd6e96cf3d33a4d35a31e527c
Ranjit Nair <58624792+ranjitmnair@users.noreply.github.com>
2020-05-29 18:26:04 +0530

    resource route updated

--- api/resources/app.py
+++ api/resources/app.py
@@ -8,7 +8,7 @@ from datetime import date
 app = Flask(__name__)
 
 
-app.config['MONGO_URI'] = "mongodb://localhost:27017/Resources"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/SignUP"
 
 mongo = PyMongo(app)
 #domains= webdev,appdev,ml,design
@@ -21,35 +21,21 @@ def upload(domain):
     link3=_json['link3']
 
     if title and (link1 or link2 or link3) and request.method=="POST":
-        today=date.today()
-        if domain=="appdev":
-            mongo.db.appdev.insert({'title':title,'date':today, 'link1':link1,'link2':link2,'link3':link3})
-        elif domain=="webdev":
-            mongo.db.webdev.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})
-        elif domain=="ml":
-            mongo.db.ml.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})
-        elif domain=="design":
-            mongo.db.design.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})
-        else:
-            mongo.db.misc.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})#misc resources
-        return jsonify(domain+" resource added")
-    else:
-        return jsonify("error") 
+        today=str(date.today())
+        mongo.db.resources.insert({'domain':domain,'title':title,'date': today,'link1':link1,'link2':link2,'link3':link3})
+        return jsonify(domain+" resource added")    
+    return jsonify("error") 
 @app.route('/resources/<domain>')
 def display(domain):
-    if domain=="appdev":
-        result=mongo.db.appdev.find()
-    elif domain=="webdev":
-        result=mongo.db.webdev.find()
-    elif domain=="ml":
-        result=mongo.db.ml.find()
-    elif domain=="design":
-        result=mongo.db.design.find()
-    else:
-        result=mongo.db.misc.find()#misc resources
-    
+    result=mongo.db.resources.find({'domain':domain})    
     response=dumps(result)
     return response       
+
+# @app.route('/')
+# def hello():
+#     return jsonify("hello user")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
