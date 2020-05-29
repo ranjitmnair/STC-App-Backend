81664aad66b14b0e2c2cf8f2ab91b923a013ff90
ranjitmnair <ranjitnair19@gmail.com>
2020-05-29 10:57:36 +0530

    resources route adde in index-py

--- index.py
+++ index.py
@@ -3,12 +3,13 @@ from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import date
 
 app = Flask(__name__)
 
 app.secret_key = "secretkey"
 
-app.config['MONGO_URI'] = "mongodb://localhost:27017/SignUP"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/Master"
 mongo = PyMongo(app)
 
 @app.route('/signup', methods=['POST'])
@@ -65,6 +66,47 @@ def posts():
         )
         return 'post added succesfully'
     return 'unsucessful'
+
+
+#resource route added
+@app.route('/resources/<domain>/upload',methods=['POST'])
+def upload(domain):
+    _json=request.json
+    title=_json['title']
+    link1=_json['link1']
+    link2=_json['link2']
+    link3=_json['link3']
+
+    if title and (link1 or link2 or link3) and request.method=="POST":
+        today=date.today()
+        if domain=="appdev":
+            mongo.db.appdev.insert({'title':title,'date':today, 'link1':link1,'link2':link2,'link3':link3})
+        elif domain=="webdev":
+            mongo.db.webdev.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})
+        elif domain=="ml":
+            mongo.db.ml.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})
+        elif domain=="design":
+            mongo.db.design.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})
+        else:
+            mongo.db.misc.insert({'title':title,'date':today,'link1':link1,'link2':link2,'link3':link3})#misc resources
+        return jsonify(domain+" resource added")
+    else:
+        return jsonify("error") 
+@app.route('/resources/<domain>')
+def display(domain):
+    if domain=="appdev":
+        result=mongo.db.appdev.find()
+    elif domain=="webdev":
+        result=mongo.db.webdev.find()
+    elif domain=="ml":
+        result=mongo.db.ml.find()
+    elif domain=="design":
+        result=mongo.db.design.find()
+    else:
+        result=mongo.db.misc.find()#misc resources
+    
+    response=dumps(result)
+    return response       
 
 
 
